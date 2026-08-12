4a0cba7305328176da9f5bb5aefeefd06f762a75
Gumballi <102613766+Gumballi@users.noreply.github.com>
2026-08-12 11:54:09 +0300

    Add owner lock functionality for commands

--- tg_bot/modules/sql/disable_sql.py
+++ tg_bot/modules/sql/disable_sql.py
@@ -18,10 +18,25 @@ class Disable(BASE):
         return "Disabled cmd {} in {}".format(self.command, self.chat_id)
 
 
+class OwnerLock(BASE):
+    __tablename__ = "owner_locked_commands"
+    chat_id = Column(String(14), primary_key=True)
+    command = Column(UnicodeText, primary_key=True)
+
+    def __init__(self, chat_id, command):
+        self.chat_id = chat_id
+        self.command = command
+
+    def __repr__(self):
+        return "Owner-locked cmd {} in {}".format(self.command, self.chat_id)
+
+
 Disable.__table__.create(checkfirst=True)
+OwnerLock.__table__.create(checkfirst=True)
 DISABLE_INSERTION_LOCK = threading.RLock()
 
 DISABLED = {}
+OWNER_LOCKED = {}
 
 
 def disable_command(chat_id, disable):
@@ -56,6 +71,46 @@ def enable_command(chat_id, enable):
         return False
 
 
+def owner_lock_command(chat_id, command):
+    with DISABLE_INSERTION_LOCK:
+        locked = SESSION.query(OwnerLock).get((str(chat_id), command))
+
+        if not locked:
+            OWNER_LOCKED.setdefault(str(chat_id), set()).add(command)
+
+            locked = OwnerLock(str(chat_id), command)
+            SESSION.add(locked)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
+
+
+def owner_unlock_command(chat_id, command):
+    with DISABLE_INSERTION_LOCK:
+        locked = SESSION.query(OwnerLock).get((str(chat_id), command))
+
+        if locked:
+            if command in OWNER_LOCKED.get(str(chat_id)):  # sanity check
+                OWNER_LOCKED.setdefault(str(chat_id), set()).remove(command)
+
+            SESSION.delete(locked)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
+
+
+def is_owner_locked(chat_id, cmd):
+    return str(cmd).lower() in OWNER_LOCKED.get(str(chat_id), set())
+
+
+def get_all_owner_locked(chat_id):
+    return OWNER_LOCKED.get(str(chat_id), set())
+
+
 def is_command_disabled(chat_id, cmd):
     return str(cmd).lower() in DISABLED.get(str(chat_id), set())
 
@@ -78,6 +133,13 @@ def num_disabled():
         SESSION.close()
 
 
+def num_owner_locked():
+    try:
+        return SESSION.query(OwnerLock).count()
+    finally:
+        SESSION.close()
+
+
 def migrate_chat(old_chat_id, new_chat_id):
     with DISABLE_INSERTION_LOCK:
         chats = SESSION.query(Disable).filter(Disable.chat_id == str(old_chat_id)).all()
@@ -87,16 +149,31 @@ def migrate_chat(old_chat_id, new_chat_id):
 
         if str(old_chat_id) in DISABLED:
             DISABLED[str(new_chat_id)] = DISABLED.get(str(old_chat_id), set())
+            del DISABLED[str(old_chat_id)]
+
+        locks = SESSION.query(OwnerLock).filter(OwnerLock.chat_id == str(old_chat_id)).all()
+        for lock in locks:
+            lock.chat_id = str(new_chat_id)
+            SESSION.add(lock)
+
+        if str(old_chat_id) in OWNER_LOCKED:
+            OWNER_LOCKED[str(new_chat_id)] = OWNER_LOCKED.get(str(old_chat_id), set())
+            del OWNER_LOCKED[str(old_chat_id)]
 
         SESSION.commit()
 
 
 def __load_disabled_commands():
     global DISABLED
+    global OWNER_LOCKED
     try:
         all_chats = SESSION.query(Disable).all()
         for chat in all_chats:
             DISABLED.setdefault(chat.chat_id, set()).add(chat.command)
+
+        all_locks = SESSION.query(OwnerLock).all()
+        for lock in all_locks:
+            OWNER_LOCKED.setdefault(lock.chat_id, set()).add(lock.command)
 
     finally:
         SESSION.close()
